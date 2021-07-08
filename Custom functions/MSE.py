91412f3c0d46beb77be4b7d9b301f9df0b8346b9
Amritpal singh <50632243+Amritpal-001@users.noreply.github.com>
2021-07-08 23:26:22 +0530

    Create MSE.py

---
+++ Custom functions/MSE.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+## Implementation 1
+def mean_saqure_error(x , y ):
+  '''
+  inputs:
+  x = actual  ( 1*n shape)
+  y = prediction( 1*n shape)
+  
+  returns:
+  MSE loss: Float 
+  
+  '''
+  diff = y - x
+  diff_squared = diff ** 2
+  mean_diff = diff_squared.mean()
+  return(mean_diff)
+  
